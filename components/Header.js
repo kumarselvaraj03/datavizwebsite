c2e4a0e843f534bb9a1ad0a715fa9b5e4f77e75a
dsudesh1991 <dsudesh1991@gmail.com>
2023-03-05 15:23:00 +0530

    first commit

---
+++ components/Header.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Image from 'next/image'
+import NavItem from "./NavItem";
+import { UserCircleIcon } from '@heroicons/react/24/solid'
+
+const MENU_LIST = [
+    { text: "Home", href: "/" },
+    { text: "Features", href: "/about" },
+    { text: "Examples", href: "/contact" },
+    { text: "Pricing", href: "/contact" },
+    { text: "Sign up", href: "/contact" },
+];
+
+function Header() {
+    const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+  return (
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-2 md:px-10'>
+        {/* Left  */}
+        <div className='relative flex items-center justify-start cursor-pointer'>
+            <Image
+            src="https://ik.imagekit.io/dataviz/dv-logo.png"
+            width={180}
+            height={52}
+            alt="DataViz"
+            />
+        </div>
+        {/* Middle  */}
+        <div className={`${navActive ? "active" : ""} nav__menu-list flex gap-4 justify-center items-center`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+        {/* Right  */}
+        <div className='flex items-center justify-end'>
+            <UserCircleIcon className="h-8 w-8 text-slate-400 cursor-pointer hover:text-slate-900" />
+        </div>
+    </header>
+  )
+}
+
+export default Header
